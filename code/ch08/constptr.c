1b2da01b01e2be3fcb911070cd0ca3c63e227ac9
suhwanxc <72860172+suhwanxc@users.noreply.github.com>
2020-11-21 23:42:28 +0900

    ch08

---
+++ code/ch08/constptr.c
@@ -0,0 +1,17 @@
+/*constptr.c*/
+#include <stdio.h>
+
+int main()
+{
+	int i = 10, j = 20;
+	const int* p = &i;
+	p = &j;
+	printf("%d\n", *p);
+
+	double d = 7.8, e = 2.7;
+	double* const pd = &d;
+	*pd = 4.4;
+	printf("%f\n", *pd);
+
+	return 0;
+}
